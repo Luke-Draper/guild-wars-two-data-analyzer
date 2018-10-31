6981a30a813f534af494d9424dc0ef1c9356cbc2
Luke-Draper <lukepdraper@gmail.com>
2018-10-31 18:56:37 -0500

    feat: Initialized Library Using typescript-library-starter

---
+++ test/guild-wars-two-data-analyzer.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from '../src/guild-wars-two-data-analyzer'
+
+/**
+ * Dummy test
+ */
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
