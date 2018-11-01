244b17b1d349b5caf4a396a60c49d62c43094817
Luke-Draper <lukepdraper@gmail.com>
2018-11-01 13:42:05 -0500

    chore: Test Axios Async Code

--- src/guild-wars-two-data-analyzer.test.ts
+++ src/guild-wars-two-data-analyzer.test.ts
@@ -1,14 +1,20 @@
 import DummyClass from './guild-wars-two-data-analyzer'
-
 /**
  * Dummy test
  */
 describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
+	it('works if true is truthy', () => {
+		expect(true).toBeTruthy()
+	})
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
+	it('DummyClass is instantiable', () => {
+		console.log('hi')
+		expect(new DummyClass()).toBeInstanceOf(DummyClass)
+	})
+	test('XHR happens', async () => {
+		expect.assertions(1)
+		const dummy = new DummyClass()
+		const output = await dummy.countBreeds()
+		await expect(output).toBe('87')
+	})
 })
--- src/guild-wars-two-data-analyzer.ts
+++ src/guild-wars-two-data-analyzer.ts
@@ -1,4 +1,29 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+const axios = require('axios')
+export default class DummyClass {
+	getBreeds = () => {
+		try {
+			return axios.get('https://dog.ceo/api/breeds/list/all')
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	countBreeds = async () => {
+		let output = ''
+		const breeds = this.getBreeds()
+			.then((response: any) => {
+				if (response.data.message) {
+					console.log(Object.entries(response.data.message).length.toString())
+					output = Object.entries(response.data.message).length.toString()
+				}
+			})
+			.catch((error: any) => {
+				console.log(error)
+			})
+			.then(() => {
+				return output.toString()
+			})
+	}
+}
+const dummy = new DummyClass()
+dummy.countBreeds()
